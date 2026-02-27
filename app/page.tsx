0a52255d7b1e9b740af077c322110ca0ea52fafb
Sai Chaitanya Gaddam <saichithanya1@gmail.com>
2026-02-28 00:31:22 +1100

    Initial commit: JobPilot SaaS application setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="flex min-h-screen flex-col bg-white">
+    <main className="flex-1">
+      {/* Hero Section */}
+    </main>
+   </div>
+  );
+}
